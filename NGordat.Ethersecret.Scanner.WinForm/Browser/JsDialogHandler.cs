9d7290b4f82051409153a51df4264b76ff2438cc
reanox <reanox@hotmail.com.tw>
2018-01-18 21:10:00 +0800

    Skip the javascript alert.

---
+++ NGordat.Ethersecret.Scanner.WinForm/Browser/JsDialogHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+
+namespace NGordat.Ethersecret.Scanner.WinForm.Browser
+{
+
+
+public class JsDialogHandler : IJsDialogHandler
+    {
+        public bool OnJSDialog(IWebBrowser browserControl, IBrowser browser, string originUrl, CefJsDialogType dialogType, string messageText, string defaultPromptText, IJsDialogCallback callback, ref bool suppressMessage)
+        {
+            callback.Continue(true);
+            return true;
+        }
+
+        public bool OnJSBeforeUnload(IWebBrowser browserControl, IBrowser browser, string message, bool isReload, IJsDialogCallback callback)
+        {
+            return true;
+        }
+
+        public void OnResetDialogState(IWebBrowser browserControl, IBrowser browser)
+        {
+
+        }
+
+        public void OnDialogClosed(IWebBrowser browserControl, IBrowser browser)
+        {
+
+        }
+    }
+}
